10b40326d6f8393ff482c10618dfca2cf8c2718b
NIMING <924150590@qq.com>
2021-04-01 15:24:52 +0800

    null

--- app/src/main/java/com/example/moduletest/MainActivity.java
+++ app/src/main/java/com/example/moduletest/MainActivity.java
@@ -1,26 +1,38 @@
 package com.example.moduletest;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 import android.view.View;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.alibaba.android.arouter.launcher.ARouter;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 @Route(path = "/Main/MainActivity")
 public class MainActivity extends AppCompatActivity {
+
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        findViewById(R.id.login).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ARouter.getInstance().build("/Login/LoginActivity").navigation();
-                //startActivity(new Intent(MainActivity.this, LoginActivity.class));
-            }
-        });
+        Init();
+        NavController navController = Navigation.findNavController(this,R.id.main_fragment);
+        NavigationUI.setupWithNavController(bottomNavigationView,navController);
+
+
+    }
+
+    private void Init(){
+
+        bottomNavigationView = findViewById(R.id.main_bar);
+
     }
 }
